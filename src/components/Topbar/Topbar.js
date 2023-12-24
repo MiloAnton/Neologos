0c2184b798d8a085ff5abe96c23a3eb8d0ba1246
Milo Roche-Vandenbroucque <milo.roche@outlook.com>
2023-12-24 09:39:19 +0100

    First commit

---
+++ src/components/Topbar/Topbar.js
@@ -0,0 +1,11 @@
+import "./Topbar.css";
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <h1 style={{ margin:"auto" }}>Neologos</h1>
+        </div>
+    )
+}
+
+export default Topbar;
---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+const data = {
+  elements: {
+    prefixes: [
+      { element: "anti", language: "Greek", meaning: "contre" },
+      { element: "hyper", language: "Greek", meaning: "au-dessus, au-delà" },
+      { element: "hypo", language: "Greek", meaning: "sous" },
+      { element: "pro", language: "Greek", meaning: "avant" },
+      { element: "sub", language: "Latin", meaning: "sous" },
+      { element: "trans", language: "Latin", meaning: "à travers" },
+      { element: "uni", language: "Latin", meaning: "un" },
+    ],
+    roots: [
+      { element: "bio", language: "Greek", meaning: "vie" },
+      { element: "geo", language: "Greek", meaning: "terre" },
+      { element: "logo", language: "Greek", meaning: "mot, étude" },
+      {
+        element: "patho",
+        language: "Greek",
+        meaning: "sentiment, souffrance",
+      },
+      { element: "aqua", language: "Latin", meaning: "eau" },
+      { element: "audi", language: "Latin", meaning: "entendre" },
+      { element: "visio", language: "Latin", meaning: "voir" },
+    ],
+    suffixes: [
+      { element: "gisme", language: "Greek", meaning: "la pratique de" },
+      { element: "logie", language: "Greek", meaning: "étude de" },
+      { element: "phobie", language: "Greek", meaning: "peur de" },
+      { element: "scope", language: "Greek", meaning: "instrument pour voir" },
+      { element: "sable", language: "Latin", meaning: "capable de" },
+      { element: "tible", language: "Latin", meaning: "capable de" },
+      { element: "tion", language: "Latin", meaning: "l'action de" },
+    ],
+  },
+};
+
+export default data;
